5e6f54a791102ad6b171075248330d40050dfc27
SAPikachu <sapikachu@gmail.com>
2012-05-08 12:19:39 +0800

    Fix route-up.sh for ppp connections

--- openvpn/route-up.sh
+++ openvpn/route-up.sh
@@ -2,9 +2,6 @@
 
 echo route-up
 
-local_net=$(echo $route_net_gateway | cut -d. -f 1-3).0
-local_route_params=$(ip route get $route_net_gateway | head -1 | grep -Eo " .*")
-local_route_params="$local_route_params proto kernel scope link"
 
 vars="ifconfig_local ifconfig_remote ifconfig_netmask local remote_1 route_net_gateway route_vpn_gateway route_network_1 route_netmask_1 route_gateway_1 local_net local_route_params"
 
@@ -13,18 +10,24 @@ for x in $vars; do
 done
 
 
-set -eu
+set -u
 
 table="table openvpn"
 
-if [ "x$table" != "x" ]; then
-    ip route flush $table
-    ip route add $local_net/24 $local_route_params $table
-fi
-ip route add $remote_1/32 via $route_net_gateway $table
+gateway_route=`ip route get $remote_1 | head -1 | sed s/$remote_1//`
+
+ip route flush $table
+
+export OPENVPN_ROUTE_TABLE="$table"
+
+# Copy all routes except default route to the table
+ip route show | grep -v ^default | awk '{ system("ip route add " $0 "$OPENVPN_ROUTE_TABLE") } '
+
+ip route add $remote_1/32 $gateway_route $table
 ip route add default via $route_vpn_gateway $table
 /etc/sapikachu/openvpn/chnroutes.sh "via $route_net_gateway $table"
 
 ip route flush cache
 service unbound restart
 
+exit 0
