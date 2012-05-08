42e931b3d18ba5cb22f437c2df3a08084d7f2ee5
SAPikachu <sapikachu@gmail.com>
2012-05-08 20:38:03 +0800

    Fail fast when there is no connection

--- openvpn/openvpn-env.sh
+++ openvpn/openvpn-env.sh
@@ -5,5 +5,5 @@ config_name=$(cat "$base/config_name")
 
 cd "$base/config"
 
-params="--cd $PWD --config $config_name --dev tun0 --script-security 2 --route-noexec --route-up $base/route-up.sh --down $base/down.sh --management 127.0.0.1 56876"
+params="--cd $PWD --config $config_name --tls-timeout 5 --dev tun0 --script-security 2 --route-noexec --route-up $base/route-up.sh --down $base/down.sh --management 127.0.0.1 56876"
 
