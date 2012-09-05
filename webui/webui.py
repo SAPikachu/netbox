0da77964ad5d1e046950299ec7065580825c77ce
SAPikachu <sapikachu@gmail.com>
2012-09-05 12:30:34 +0800

    Stub for connection list page

--- webui/webui.py
+++ webui/webui.py
@@ -14,8 +14,9 @@ register_imported_modules(
 )
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/<name>")
+def page(name=""):
+    return render_template((name or "index") + ".html")
 
 if __name__ == '__main__':
     app.run(debug=True)
