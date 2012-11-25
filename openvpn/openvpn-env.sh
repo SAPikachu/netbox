82f414f27faade9b4ab8c169e1bb2fee13586816
SAPikachu <sapikachu@gmail.com>
2012-11-25 13:43:41 +0800

    Allow specifying extra openvpn parameters

--- openvpn/openvpn-env.sh
+++ openvpn/openvpn-env.sh
@@ -2,8 +2,13 @@
 
 
 config_name=$(cat "$base/config_name")
+extra_params=
+
+if [ -f "$base/extra_params" ]; then
+    extra_params=$(cat "$base/extra_params")
+fi
 
 cd "$base/config"
 
-params="--cd $PWD --config $config_name --tls-timeout 5 --dev tun0 --script-security 2 --route-noexec --route-up $base/route-up.sh --down $base/down.sh --management 127.0.0.1 56876"
+params="--cd $PWD --config $config_name --tls-timeout 5 --dev tun0 --script-security 2 --route-noexec --route-up $base/route-up.sh --down $base/down.sh --management 127.0.0.1 56876 $extra_params"
 
