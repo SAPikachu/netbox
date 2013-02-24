9c15eaa56e20ad2dc7146c0cd4f167d74c9dd270
SAPikachu <sapikachu@gmail.com>
2013-02-24 19:19:10 +0800

    Update IPv6 tunnel client address automatically

--- openvpn/post-openvpn-up.sh
+++ openvpn/post-openvpn-up.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+PREFIX=$(dirname $(readlink -m $0))
+. $PREFIX/../conf.sh
+
 service unbound restart
 [ -f /usr/sbin/pdnsd-ctl ] && /usr/sbin/pdnsd-ctl empty-cache 2>&1
 initctl restart squid3
 sleep 5
 ddclient
+wget -q -O - "https://ipv4.tunnelbroker.net/nic/update?username=$HE_TUNNEL_USERNAME&password=$HE_TUNNEL_PASSWORD&hostname=$HE_TUNNEL_ID"
