e92c4bdd7681c374bb850cf633e04bea7c7b2909
SAPikachu <sapikachu@gmail.com>
2013-03-20 18:58:54 +0800

    webui: Fix connection output and add IPv6 support

--- webui/modules/connections.py
+++ webui/modules/connections.py
@@ -1,6 +1,8 @@
 from __future__ import division, print_function, unicode_literals
 
 import re
+import os
+import subprocess
 
 from module_base import Module
 
@@ -8,10 +10,14 @@ class Connections(Module):
     def __init__(self, config):
         pass
 
-    def _parse_peer(self, parts):
+    def _parse_peer(self, proto, parts):
         peer = {}
-        for _ in range(4):
-            m = re.match(r"^(src|dst|sport|dport)=(.+)$", parts.pop(0))
+        fields = ["src", "dst"]
+        if proto in ("tcp", "udp"):
+            fields += ["sport", "dport"]
+
+        for _ in range(len(fields)):
+            m = re.match(r"^({})=(.+)$".format("|".join(fields)), parts.pop(0))
             key = m.group(1)
             value = m.group(2)
             if key.endswith("port"):
@@ -27,35 +33,42 @@ class Connections(Module):
 
         return peer
     
+
     def get_connections(self):
+        return self._get_connections_impl("ipv4") + \
+               self._get_connections_impl("ipv6")
+    
+    def _get_connections_impl(self, family):
         connections = []
-        with open("/proc/net/nf_conntrack", "r") as f:
-            for line in f:
-                line = line.strip()
-                if not line:
-                    continue
+        p = subprocess.Popen(["sudo", "-n", "conntrack", "-L", "-f", family],
+                            stdout=subprocess.PIPE,
+                            close_fds=True,)
+        for line in p.stdout:
+            line = line.strip()
+            if not line:
+                continue
 
-                parts = re.split(r"\s+", line)
-                conn = {}
+            parts = re.split(r"\s+", line)
+            conn = {}
 
-                if parts[0].startswith("ipv"):
-                    conn["version"] = parts[0]
-                    parts = parts[2:]
-
-                conn["protocol"] = parts[0]
+            if parts[0].startswith("ipv"):
+                conn["version"] = parts[0]
                 parts = parts[2:]
 
-                conn["timeout"] = int(parts.pop(0))
+            conn["protocol"] = parts[0]
+            parts = parts[2:]
 
-                if not parts[0].startswith("src="):
-                    conn["state"] = parts.pop(0)
+            conn["timeout"] = int(parts.pop(0))
 
-                conn["tx"] = self._parse_peer(parts)
-                if parts[0].startswith("["):
-                    conn["state2"] = parts.pop(0)[1:-1]
+            if not parts[0].startswith("src="):
+                conn["state"] = parts.pop(0)
 
-                conn["rx"] = self._parse_peer(parts)
+            conn["tx"] = self._parse_peer(conn["protocol"], parts)
+            if parts[0].startswith("["):
+                conn["state2"] = parts.pop(0)[1:-1]
 
-                connections.append(conn)
+            conn["rx"] = self._parse_peer(conn["protocol"], parts)
+
+            connections.append(conn)
                 
         return connections
