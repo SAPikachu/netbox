c19dc0169df761a1946a6f3cc4190a286d162ad4
SAPikachu <sapikachu@gmail.com>
2013-04-28 18:14:14 +0800

    chn_networks: Update network list, and speed up ipset initialization significantly

--- vpnutils/ipset-chn-networks.sh
+++ vpnutils/ipset-chn-networks.sh
@@ -9,5 +9,14 @@ if [ "x$1" == "x--empty-set-only" ]; then
     exit 0;
 fi
 
-cat $(dirname "$(readlink -m "$0")")/chn_networks.txt | xargs -n 1 ipset add $SET_NAME
+PREFIX=$(dirname "$(readlink -m "$0")")
+RAW_NET_LIST=$PREFIX/chn_networks.txt
+IPSET_RULE_FILE=$PREFIX/chn_networks.ipsetrules
+
+if [  -f $IPSET_RULE_FILE ] && [ $IPSET_RULE_FILE -nt $RAW_NET_LIST ]; then
+    cat $IPSET_RULE_FILE | grep -v "^create" | ipset restore
+else 
+    cat $RAW_NET_LIST | xargs -n 1 ipset add $SET_NAME
+    ipset save $SET_NAME > $IPSET_RULE_FILE
+fi
 
---
+++ vpnutils/update_chn_networks.sh
@@ -0,0 +1,3 @@
+./chnroutes_build.py "{ip}/{mask}" 0 > chn_networks.txt
+./ipset-chn-networks.sh
+
