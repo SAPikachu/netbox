f3a6c919b67fb54106706c4a8d1c7c1faa20eba7
SAPikachu <sapikachu@gmail.com>
2013-06-15 15:40:28 +0800

    conf_files: Should do substitution on 50unattended-upgrades

--- conf_files/build.sh
+++ conf_files/build.sh
@@ -15,22 +15,26 @@ for template in ^*; do
         mv $TARGET /tmp/$(basename $TARGET).bak
     fi
     cp --attributes-only $template $TARGET
-    cat $template | {
-        linenum=1
-        while read line; do
-            while [[ $line =~ \$\{([a-zA-Z_][a-zA-Z_0-9]*)\} ]]; do
-                LHS=${BASH_REMATCH[1]}
-                RHS="$(eval echo "\"\${$LHS-}\"")"
-                if [ -z $RHS ]; then
-                    echo "Warning: $TARGET line $linenum: Undefined variable \"$LHS\""
-                    RHS="[UNDEFINED]"
-                fi
-                line=${line//\$\{$LHS\}/$RHS}
+    if grep @@@-COPY-ONLY $template > /dev/null; then
+        grep -v @@@-COPY-ONLY $template > $TARGET
+    else
+        cat $template | {
+            linenum=1
+            while read line; do
+                while [[ $line =~ \$\{([a-zA-Z_][a-zA-Z_0-9]*)\} ]]; do
+                    LHS=${BASH_REMATCH[1]}
+                    RHS="$(eval echo "\"\${$LHS-}\"")"
+                    if [ -z $RHS ]; then
+                        echo "Warning: $TARGET line $linenum: Undefined variable \"$LHS\""
+                        RHS="[UNDEFINED]"
+                    fi
+                    line=${line//\$\{$LHS\}/$RHS}
+                done
+                echo $line >> $TARGET
+                let linenum+=1
             done
-            echo $line >> $TARGET
-            let linenum+=1
-        done
-    }
+        }
+    fi
     chmod --reference=$template $TARGET
     chown --reference=$template $TARGET
 done
