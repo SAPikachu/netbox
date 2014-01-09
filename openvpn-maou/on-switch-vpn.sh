944b405fdc52dd818889c7b8fdf99f0137d42325
SAPikachu <sapikachu@gmail.com>
2014-01-09 08:44:45 +0800

    Include DHCP hosts in DNS

--- openvpn-maou/on-switch-vpn.sh
+++ openvpn-maou/on-switch-vpn.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+PREFIX=$(dirname $(readlink -m $0))
+. $PREFIX/../conf.sh
+
 [ -f /usr/sbin/pdnsd-ctl ] && /usr/sbin/pdnsd-ctl empty-cache 2>&1
+[ -f /usr/sbin/pdnsd-ctl ] && $PREFIX/../pdnsd/update-dns.sh 2>&1
 initctl restart squid3
 sleep 5
 ddclient
--- openvpn/post-openvpn-up.sh
+++ openvpn/post-openvpn-up.sh
@@ -5,6 +5,7 @@ PREFIX=$(dirname $(readlink -m $0))
 
 service unbound restart
 [ -f /usr/sbin/pdnsd-ctl ] && /usr/sbin/pdnsd-ctl empty-cache 2>&1
+[ -f /usr/sbin/pdnsd-ctl ] && $PREFIX/../pdnsd/update-dns.sh 2>&1
 initctl restart squid3
 sleep 5
 ddclient
